f65c812985062928d42a606a2b0f3d11fa459ed0
014967 <boris@xyzcorp.io>
2024-08-21 01:15:26 +0900

    생일축하 Activity 팝업 구현

--- app/src/main/java/com/mashup/ui/webview/WebViewViewModel.kt
+++ app/src/main/java/com/mashup/ui/webview/WebViewViewModel.kt
@@ -5,22 +5,37 @@ import androidx.lifecycle.viewModelScope
 import com.mashup.constant.EXTRA_TITLE_KEY
 import com.mashup.constant.EXTRA_URL_KEY
 import com.mashup.core.common.base.BaseViewModel
+import com.mashup.core.data.repository.PopUpRepository
 import com.mashup.data.network.WEB_HOST
 import com.mashup.datastore.data.repository.UserPreferenceRepository
+import com.mashup.ui.main.model.MainPopupType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class WebViewViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
-    userPreferenceRepository: UserPreferenceRepository
+    private val savedStateHandle: SavedStateHandle,
+    userPreferenceRepository: UserPreferenceRepository,
+    private val popUpRepository: PopUpRepository,
 ) : BaseViewModel() {
 
     private val showDividerFlow = MutableStateFlow(false)
+    private val _showPopupType = MutableSharedFlow<MainPopupType>()
+    val showPopupType: SharedFlow<MainPopupType> = _showPopupType.asSharedFlow()
+
+    private val _onClickPopupConfirm = MutableSharedFlow<MainPopupType>()
+    val onClickPopupConfirm: SharedFlow<MainPopupType> = _onClickPopupConfirm.asSharedFlow()
+
+    init {
+        getBirthDayPopup()
+    }
 
     val webViewUiState = combine(
         savedStateHandle.getStateFlow(EXTRA_TITLE_KEY, ""),
@@ -49,6 +64,36 @@ class WebViewViewModel @Inject constructor(
     }
 
     override fun handleErrorCode(code: String) {
+    }
+
+    private fun getBirthDayPopup() = mashUpScope {
+        val result = popUpRepository.getPopupKeyList()
+        val url = savedStateHandle.getStateFlow(EXTRA_URL_KEY, "").value
+
+        if(url == "birthday/event") {
+           return@mashUpScope
+        }
+
+
+        if(result.isSuccess()){
+            val popupType =
+                MainPopupType.getMainPopupType(result.data?.firstOrNull() ?: MainPopupType.BIRTHDAY_CELEBRATION.name)
+            if (popupType != MainPopupType.BIRTHDAY_CELEBRATION) return@mashUpScope
+            _showPopupType.emit(popupType)
+        }
+    }
+
+    fun disablePopup(popupKey: MainPopupType) = mashUpScope {
+        if (popupKey == MainPopupType.UNKNOWN) return@mashUpScope
+        popUpRepository.patchPopupDisabled(popupKey.name)
+    }
+
+
+    fun onClickPopup(popupKey: String) = mashUpScope {
+        val popupType =
+            MainPopupType.getMainPopupType(popupKey)
+        if (popupType == MainPopupType.UNKNOWN) return@mashUpScope
+        _onClickPopupConfirm.emit(popupType)
     }
 }
 
--- app/src/main/java/com/mashup/ui/webview/birthday/BirthdayActivity.kt
+++ app/src/main/java/com/mashup/ui/webview/birthday/BirthdayActivity.kt
@@ -8,28 +8,71 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.fragment.NavHostFragment
+import com.mashup.R
+import com.mashup.base.BaseActivity
 import com.mashup.constant.EXTRA_TITLE_KEY
 import com.mashup.constant.EXTRA_URL_KEY
 import com.mashup.core.common.bridge.MashupBridge
+import com.mashup.core.common.utils.safeShow
 import com.mashup.core.ui.theme.MashUpTheme
+import com.mashup.databinding.ActivityBirthdayBinding
+import com.mashup.ui.main.model.MainPopupType
+import com.mashup.ui.main.popup.MainBottomPopup
 import com.mashup.ui.webview.WebViewScreen
 import com.mashup.ui.webview.WebViewUiState
 import com.mashup.ui.webview.WebViewViewModel
 import com.mashup.util.setFullScreen
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class BirthdayActivity : ComponentActivity() {
+class BirthdayActivity : BaseActivity<ActivityBirthdayBinding>() {
+
+    override val layoutId = R.layout.activity_birthday
 
     private val webViewViewModel by viewModels<WebViewViewModel>()
+
+    override fun initObserves() {
+        super.initObserves()
+        flowLifecycleScope {
+            launch{
+                webViewViewModel.showPopupType.collectLatest {
+                    MainBottomPopup.newInstance(it).safeShow(supportFragmentManager)
+                }
+            }
+
+            launch{
+                webViewViewModel.onClickPopupConfirm.collectLatest { popupType ->
+                    when (popupType) {
+                        MainPopupType.BIRTHDAY_CELEBRATION -> {
+                            webViewViewModel.disablePopup(popupType)
+                            startActivity(
+                                newIntent(
+                                    context = this@BirthdayActivity,
+                                    urlKey = "birthday/event"
+                                )
+                            )
+                        }
+                        else->{}
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             MashUpTheme {
                 val webViewUiState by webViewViewModel.webViewUiState.collectAsState(WebViewUiState.Loading)
@@ -37,7 +80,7 @@ class BirthdayActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     webViewUiState = webViewUiState,
                     mashupBridge = MashupBridge(
-                        this,
+                        this@BirthdayActivity,
                         onBackPressed = ::finish
                     ),
                     isShowMashUpToolbar = false
